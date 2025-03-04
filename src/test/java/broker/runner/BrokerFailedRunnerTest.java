ca1c427fc457c311f0632a7bd089c09be8b8bf1e
Rajan Sardana <rajansardanakx>
2025-03-04 13:38:56 +0530

    changes

--- src/test/java/broker/runner/BrokerFailedRunnerTest.java
+++ src/test/java/broker/runner/BrokerFailedRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"@target/rerun.txt"},
 
-        glue = {"broker/runner/stepdefinations","hook"}, plugin = {"pretty", "json:target/cucumber-reports/cucumber-rerun.json" },tags = "@Automated",monochrome = true )
+        glue = {"broker/stepdefination","hook"}, plugin = {"pretty", "json:target/cucumber-reports/cucumber-rerun.json" },tags = "@Automated",monochrome = true )
 public class BrokerFailedRunnerTest {
 
 
